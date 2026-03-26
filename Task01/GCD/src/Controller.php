4b26dd638e3fb833cfe142d5f894bbc87eb05edc
JuliaKulav <yuliya29052001@mail.ru>
2026-03-26 11:12:03 +0300

    Task01: улучшенная версия игры НОД с 3 раундами

--- Task01/GCD/src/Controller.php
+++ Task01/GCD/src/Controller.php
@@ -5,7 +5,9 @@ namespace JuliaKulav\Gcd\Controller;
 use function JuliaKulav\Gcd\View\showWelcome;
 use function JuliaKulav\Gcd\View\showQuestion;
 use function JuliaKulav\Gcd\View\showResult;
-use function cli\prompt;
+use function JuliaKulav\Gcd\View\showGameEnd;
+use function JuliaKulav\Gcd\View\showFarewell;
+use function JuliaKulav\Gcd\View\askQuestion;
 use function cli\line;
 
 function gcd(int $a, int $b): int
@@ -22,20 +24,33 @@ function startGame(): void
 {
     showWelcome();
 
-    $name = prompt("May I have your name?");
-    line("Hello, %s!", $name);
+    $name = askQuestion("Представьтесь, пожалуйста");
+    line("\nДобро пожаловать, %s!", $name);
+    line("Вам нужно ответить на 3 вопроса.\n");
 
-    $a = rand(1, 100);
-    $b = rand(1, 100);
+    $score = 0;
+    $totalRounds = 3;
 
-    showQuestion($a, $b);
-
-    $answer = prompt("Your answer");
-    $correct = gcd($a, $b);
-
-    if ((int)$answer === $correct) {
-        showResult(true, $correct);
-    } else {
-        showResult(false, $correct);
+    for ($round = 1; $round <= $totalRounds; $round++) {
+        line("Раунд %d из %d", $round, $totalRounds);
+        
+        $a = rand(10, 99);
+        $b = rand(10, 99);
+        
+        showQuestion($a, $b);
+        
+        $answer = askQuestion("Ваш ответ");
+        $correct = gcd($a, $b);
+        
+        if ((int)$answer === $correct) {
+            showResult(true, $correct);
+            $score++;
+        } else {
+            showResult(false, $correct);
+            break;
+        }
     }
+    
+    showGameEnd($name, $score, $totalRounds);
+    showFarewell($name);
 }
--- Task01/GCD/src/View.php
+++ Task01/GCD/src/View.php
@@ -3,22 +3,64 @@
 namespace JuliaKulav\Gcd\View;
 
 use function cli\line;
+use function cli\prompt;
 
 function showWelcome(): void
 {
-    line("Welcome to the GCD Game!");
+    line("\n");
+    line("==================================================");
+    line("                ИГРА: НАЙДИ НОД");
+    line("           Наибольший Общий Делитель");
+    line("==================================================");
+    line("\nДобро пожаловать в математическую игру!");
+    line("Правила: найдите наибольший общий делитель двух чисел.");
+    line("Пример: НОД(12, 18) = 6");
+    line("==================================================\n");
 }
 
 function showQuestion(int $a, int $b): void
 {
-    line("Find the greatest common divisor of %d and %d.", $a, $b);
+    line("\n--------------------------------------------------");
+    line("Вопрос: НОД(%d, %d) = ?", $a, $b);
+    line("--------------------------------------------------");
 }
 
 function showResult(bool $isCorrect, int $correct): void
 {
     if ($isCorrect) {
-        line("Correct!");
+        line("\n[ВЕРНО] Правильный ответ!");
+        line("------------------------------------------\n");
     } else {
-        line("Wrong answer. Correct answer: %d", $correct);
+        line("\n[НЕВЕРНО] Правильный ответ: %d", $correct);
+        line("------------------------------------------\n");
     }
+}
+
+function showGameEnd(string $name, int $score, int $total): void
+{
+    line("\n==================================================");
+    line("                  РЕЗУЛЬТАТ");
+    line("==================================================");
+    line("Игрок: %s", $name);
+    line("Правильных ответов: %d из %d", $score, $total);
+    
+    if ($score == $total) {
+        line("Поздравляю! Вы победили!");
+    } elseif ($score >= $total / 2) {
+        line("Хороший результат!");
+    } else {
+        line("Попробуйте еще раз!");
+    }
+    line("==================================================\n");
+}
+
+function showFarewell(string $name): void
+{
+    line("Спасибо за игру, %s!", $name);
+    line("Для повторного запуска введите: php bin/gcd\n");
+}
+
+function askQuestion(string $prompt): string
+{
+    return prompt($prompt);
 }
